328291a698cd8e8027b6a0aa93ff1155061478bd
raskrutovstudio-collab <raskrutovstudio@gmail.com>
2026-09-15 12:57:44 +0600

    build(lucerne): bump hero layout asset version

--- site/assets/js/product-lucerne-use-icons.js
+++ site/assets/js/product-lucerne-use-icons.js
@@ -1,7 +1,7 @@
 (() => {
   if (!document.body.classList.contains('page-lucerne-v3')) return;
 
-  const version = '20260915-7';
+  const version = '20260915-8';
 
   const ensureStyle = (href, marker) => {
     if (document.querySelector(`link[${marker}]`)) return;
